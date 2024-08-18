0aaf9774d5dd15dab2577fd90c545101e53bb24f
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-08-18 22:35:46 +0900

    test用の Table作成SQLに追加

--- packages/core/db/test.sql
+++ packages/core/db/test.sql
@@ -1,4 +1,11 @@
 -- このファイルはテスト用のデータベーススキーマです。
 
+-- テストユーザーを作成
 INSERT INTO users (username, email, password_hash)
 VALUES ('test', 'test@test.com', 'test');
+
+-- テストコレクションを作成
+INSERT INTO collections (slug, label, description, access, default_sort, list_searchable_fields)
+VALUES ('test', 'test', 'test', true, 'created_at', 'created_at');
+
+
