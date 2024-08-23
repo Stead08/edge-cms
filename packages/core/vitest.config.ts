d1fc0623fe5800fa1db66fa354db12b8ad395c02
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-08-23 14:42:21 +0900

    chore (core): テスト用wrangler.tomlリネームに伴い、vitestの設定を修正した

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -17,7 +17,7 @@ export default defineWorkersProject(async () => {
 				workers: {
 					singleWorker: true,
 					wrangler: {
-						configPath: "./wrangler.toml",
+						configPath: "./wrangler.test.toml",
 						environment: "production",
 					},
 					miniflare: {
