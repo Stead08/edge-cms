64aaccff043f74c7b01371af85c38b79c4ec845e
Stead08 <62312581+Stead08@users.noreply.github.com>
2024-09-15 14:15:22 +0900

    feat 認証認可関係の実装 #21 : prisma 導入

--- packages/auth/db/schema.sql
+++ packages/auth/db/schema.sql
@@ -8,5 +8,7 @@ create table sessions
 (
     id         TEXT    not null primary key,
     expires_at INTEGER not null,
-    user_id    TEXT    not null
+    user_id    TEXT    not null,
+    foreign key (user_id) references users (id)
 );
+
